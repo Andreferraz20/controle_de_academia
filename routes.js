f5d538a0766500ae60995dda5c8487fc1300cd23
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-01-06 15:04:54 +0000

    add  Create Instructor e validação

--- routes.js
+++ routes.js
@@ -8,6 +8,23 @@ routes.get('/instructors',  (req, res) =>
     res.render("instructors/index")
 );
 
+routes.get('/instructors/create',  (req, res) => 
+    res.render("instructors/create")
+);
+
+routes.post('/instructors',  (req, res) => {
+
+    const keys = Object.keys(req.body);
+
+    for(key of keys) {
+        if(req.body[key] == ""){
+            return res.send("Fill all fields");
+        }
+    }
+
+    return res.send(req.body);
+});
+
 routes.get('/members',  (req, res) => 
     res.send("members")
 );
